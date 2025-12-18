0e160943af96800f79500a7c1cdf104b32f22d1e
MusaBangash <mmkb5055@gmail.com>
2025-12-18 14:40:12 +0500

    add convert

---
+++ conver.py
@@ -0,0 +1,40 @@
+
+
+
+
+"""
+Docstring for convert
+"""
+
+print("Converter")
+
+
+user_amount = float(input("Enter your amount in million:"))
+
+
+
+
+#Lac
+
+"""
+1 million = 10
+
+
+"""
+
+print("Amount in lacs", user_amount*10)
+
+"""
+Thousands
+
+1 million = 10*100 lac
+1 = 100 thousand
+2 -200 thou
+
+
+"""
+
+print("Amount in thousands",user_amount*10*100*100)
+
+
+
