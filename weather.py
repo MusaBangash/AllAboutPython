abaf787e22ca6a8a9b1e55e25e51ef97efa93b27
MusaBangash <mmkb5055@gmail.com>
2025-12-18 15:12:20 +0500

    add hospital

---
+++ weather.py
@@ -0,0 +1,46 @@
+
+
+
+departments = ["Gernal","Emergency","cardiolgoy","Dermatology","Nureal"]
+
+print("Services Hospital")
+
+
+
+def patient_profile():
+    p_name = input("Enter patient name:")
+    p_age = int(input("Enter patient age:"))
+    p_gender = input("Enter patient gender:")
+    p_diease = input("Enter patient diease:")
+
+
+def recipt():
+    patient_number = input("Enter patient number:")
+
+    return patient_number
+
+
+print("Reciption Patient Profile")
+patient_profile()
+
+
+print(recipt())
+
+
+
+p_d_information = []
+
+i=1
+while True:
+
+    doctor_question = input("Doctor Question:")
+    if doctor_question == "R":
+        break
+    patient_answer = input("Patient Answer:")
+    p_d_information.append(patient_answer)
+
+
+
+
+
+
